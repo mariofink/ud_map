70a75b66d94b20670428420488517d886fd199d1
Mario Fink <mario.fink@goodyear.com>
2018-04-06 23:54:00 +0200

    use Webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
   mode: "development",
   entry: "./js/src/index.js",
   devtool: "inline-source-map",
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  },
   output: {
     filename: "index-built.js",
     path: path.resolve(__dirname, "js/dist")
