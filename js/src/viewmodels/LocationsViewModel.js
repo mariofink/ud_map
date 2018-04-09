ed7ccfbb31c0145b43899a3ab1f6bdc0d80ba0b8
Mario Fink <mario.fink@goodyear.com>
2018-04-09 13:49:34 +0200

    add location list filter

--- js/src/viewmodels/LocationsViewModel.js
+++ js/src/viewmodels/LocationsViewModel.js
@@ -1,7 +1,25 @@
+import _ from "lodash";
+
 export default class LocationsViewModel {
   constructor() {
     this.locations = ko.observable();
+    this.filterInput = ko.observable();
+    this.filteredList = ko.computed(
+      () => this.getFilteredList(this.filterInput()),
+      this
+    );
     this.init();
+  }
+  getFilteredList(input) {
+    // if a filter query has been entered, return the filtered list of locations
+    if (typeof input === "string" && input.length > 0) {
+      return _.filter(this.locations(), location => {
+        // filter case insensitive
+        return new RegExp(input, "i").test(location.name);
+      });
+    }
+    // otherwise return all locations
+    return this.locations();
   }
   setLocations(locations) {
     this.locations(locations);
