cd0112d5f90d8088b0c31c5881e8a9cadd8df0c9
Mario Fink <mario.fink@goodyear.com>
2018-04-23 09:07:53 +0200

    add error handling when Maps API could not be loaded

--- js/src/index.js
+++ js/src/index.js
@@ -2,6 +2,10 @@ import GoogleMapsService from "./services/GoogleMapsService";
 import LocationsViewModel from "./viewmodels/LocationsViewModel";
 
 window.initMap = () => {
+  /**
+   * Initialises the Google Maps Api and loads a list of restaurants for the specified location.
+   * It then puts this list into the locations view model that is handled by knockout.
+   */
   const myMapService = new GoogleMapsService(google.maps, {
     location: { lat: 50.5512631, lng: 9.6752945 },
     element: document.getElementById("map")
@@ -17,4 +21,11 @@ window.initMap = () => {
       locationsViewModel.setLocations(results);
     });
   });
+};
+
+window.onMapsError = () => {
+  /**
+   * In case the Google Maps API could not be loaded, show the error message.
+   */
+  document.getElementById("maps-error").style.display = "block";
 };
